af2af854bafcf97ed672fd4c8283ebab7a3afca5
Danny Garcia <dgarcia1724@berkeley.edu>
2023-12-05 19:33:07 -0800

    114. fresh start

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// pages & components
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
 function App() {
   return (
     <div className="App">
-      <h1>lol</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
