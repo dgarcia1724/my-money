5169dd7bda390f5ac9f5d368a167e6219fbb02f4
Danny Garcia <dgarcia1724@berkeley.edu>
2023-12-05 20:13:08 -0800

    115. fresh start

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+// styles
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>myMoney</li>
+        <li>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
